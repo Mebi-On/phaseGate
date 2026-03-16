4e8f91aec52cb2db10c272875a40649b0fdc1a1a
Mebi-On <dumebionwusah@gmail.com>
2026-03-16 17:13:29 +0100

    Day 1

---
+++ javaScript/Gate One/task_five.js
@@ -0,0 +1,5 @@
+let celsius_temp = 1
+let fahrenheit = celsius_temp * 9/5 + 32
+
+console.log("The temperature is", fahrenheit + "F")
+ 
---
+++ javaScript/Gate One/task_three.js
@@ -0,0 +1,4 @@
+let numberOne = 12
+let numberTwo = 20
+
+console.log("The sum of both numbers is", (numberOne + numberTwo)) 
